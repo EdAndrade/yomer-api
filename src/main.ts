7458c1fa5894471788ec88c6be05b3d78e9cd84a
edAndrade <edmilisonwork@gmail.com>
2023-04-29 20:02:13 +0100

    removed code to test wacthtower --- with success

--- src/main.ts
+++ src/main.ts
@@ -22,9 +22,5 @@ async function bootstrap() {
   SwaggerModule.setup('doc', app, SwaggerDocument);
 
   await app.listen(3000);
-
-  setInterval(() => {
-    console.log('for watchtower test')
-  },1000)
 }
 bootstrap();
